350362001be247bcef5f55ca6399a8a9724fd3d5
SAMEER SHEKH <khansameer7311@gmail.com>
2024-07-16 23:20:20 +0530

    add cors

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './src/db/connection.js';
-
+import cors from 'cors';
+import router from './src/routes/user.routes.js';
 
 dotenv.config();
 
@@ -9,19 +10,15 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(cors()); // Enable CORS
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-import router from './src/routes/user.routes.js';
-
-app.use("/api",router);
+app.use("/api", router);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
 connectDB();
-
-
-
